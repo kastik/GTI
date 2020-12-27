def3da1a0ebf745f57fe2eb43d2c9557f8a45844
kastik <kostas21422112@gmail.com>
2020-12-27 18:48:50 +0200

    Add 9th & 10th Lab

--- NotSubmitted/Ergastirio10/10thLabScripts.js
+++ NotSubmitted/Ergastirio10/10thLabScripts.js
@@ -35,8 +35,14 @@ function startSlideWithCustomInterval(){
 function stopTimeout(){
     clearTimeout(timeout)
 }
+//TODO Lab 3
 
-//######################## Lab 3 ###############################
+
+
+
+
+
+//######################## Lab 4 ###############################
 
 function modifyATags(){
     var linkCollection = document.getElementsByTagName("a")
@@ -48,5 +54,29 @@ function modifyATags(){
 function confirmExit(event){
     if(!confirm("Do you want to exit this website?")){
         event.preventDefault()
+    }
+}
+
+function lab5(){
+    var tableObj = document.getElementById("myTable")
+    var rows = document.getElementById("rows").value
+    var cols = document.getElementById("cols").value
+    for(var i=0; i<rows; i++){
+        tableObj.insertRow(i)
+        for(var j=0; j<cols; j++){
+            tableObj.rows[i].insertCell(j);
+            tableObj.rows[i].cells.item(j).innerText = "Row "+i+" Cell "+j
+        }
+    }
+}
+function deleteLastRow(){
+    var tableObj = document.getElementById("myTable")
+    tableObj.deleteRow(tableObj.rows.length-1)
+}
+function deleteLastCol(){
+    var tableObj = document.getElementById("myTable")
+    for(var i=0; i<tableObj.rows.length; i++){
+        console.log(tableObj.rows[i].cells.length)
+        tableObj.rows[i].deleteCell(tableObj.rows[i].cells.length-1)
     }
 }
